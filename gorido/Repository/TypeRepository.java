de2189aa9f65c6baaeecbcb684c347bf738fbf78
IT25102679 <it25102679@my.sliit.lk>
2026-05-18 21:49:37 +0530

    Delete gorido/Repository/TypeRepository.java

--- gorido/Repository/TypeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.gorido.Repository;
-import com.example.gorido.Model.Type;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TypeRepository extends JpaRepository<Type, Integer> {
-}

6d1744066a9f494bd51a64a3b8438472435425fc
IT25102679 <it25102679@my.sliit.lk>
2026-05-18 23:15:31 +0530

    Add files via upload
    Updated driverService File

--- gorido/Service/DriverService.java
+++ gorido/Service/DriverService.java
@@ -1,9 +1,17 @@
 package com.example.gorido.Service;
 import com.example.gorido.DTO.DriverRegisterRequest;
+import jakarta.mail.Multipart;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
 
 public interface DriverService {
     String loadUser(HttpSession session);
     String getAllDistricts();
     String registerDriver(DriverRegisterRequest request);
+    String driverProfile(Model model, HttpSession session);
+    String deleteDriver(HttpSession session);
+    String updatelicense(LocalDate license_exp_date, MultipartFile licenseImage, HttpSession session);
 }

00d3f0d8b531494abe3320a4112986514872fe8d
IT25102679 <it25102679@my.sliit.lk>
2026-05-18 23:35:30 +0530

    Add files via upload
    Updated version

--- gorido/Repository/DriverRepository.java
+++ gorido/Repository/DriverRepository.java
@@ -1,9 +1,11 @@
 package com.example.gorido.Repository;
 import com.example.gorido.Model.Driver;
+import com.example.gorido.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface DriverRepository extends JpaRepository<Driver, Integer> {
     Optional<Driver> findByUserId(User user);
+
 }

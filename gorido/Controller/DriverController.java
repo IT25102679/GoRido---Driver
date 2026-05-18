3024f71bd706d7fde8612645db57af8718f3f582
IT25102679 <it25102679@my.sliit.lk>
2026-05-19 00:00:23 +0530

    Add files via upload
    New Version

--- gorido/Controller/DriverController.java
+++ gorido/Controller/DriverController.java
@@ -1,9 +1,14 @@
 package com.example.gorido.Controller;
 import com.example.gorido.DTO.DriverRegisterRequest;
 import com.example.gorido.Service.DriverService;
+import jakarta.mail.Multipart;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
 
 @Controller
 public class DriverController {
@@ -14,8 +19,17 @@ public class DriverController {
     }
 
     @GetMapping("/driverregi")
-    public String driverRegiPage() {
+    public String driverRegiPage(HttpSession session) {
+        String email = (String) session.getAttribute("userEmail");
+        if (email == null){
+            return "signin";
+        }
         return "driverregi";
+    }
+
+    @GetMapping("/driverprofile")
+    public String profile(Model model, HttpSession session) {
+        return driverService.driverProfile(model, session);
     }
 
     @RestController
@@ -37,4 +51,19 @@ public class DriverController {
         }
 
     }
+
+    @RestController
+    public class delDriver {
+        @GetMapping("/delete/driver")
+        public String deleteDriver(HttpSession session) {
+            return driverService.deleteDriver(session);
+        }
+    }
+
+    @ResponseBody
+    @PostMapping("/updatelicense")
+    public String updatelicense(@RequestParam LocalDate license_exp_date, @RequestParam MultipartFile licenseImage, HttpSession session){
+        return driverService.updatelicense(license_exp_date, licenseImage, session);
+    }
+
 }

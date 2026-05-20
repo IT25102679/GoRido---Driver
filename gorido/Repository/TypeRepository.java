069d684056132ee941a524484f8656c50ca6f0a5
IT25102679 <it25102679@my.sliit.lk>
2026-05-20 23:23:43 +0530

    Add files via upload

---
+++ gorido/Repository/TypeRepository.java
@@ -0,0 +1,6 @@
+package com.example.gorido.Repository;
+import com.example.gorido.Model.Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeRepository extends JpaRepository<Type, Integer> {
+}
